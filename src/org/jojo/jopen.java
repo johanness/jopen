ea634d1487d65afd6d12a1642285d36774a24b74
Johannes Strampe <johannesstrampe@hotmail.com>
2011-02-10 19:18:21 +0100

    adds empty action

---
+++ src/org/jojo/jopen.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jojo;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public final class jopen implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+    }
+}
